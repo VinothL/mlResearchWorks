d7cad36c1d9155b627fd1709b8093c311025c189
Vinoth Loganathan <vinoth@Vinoths-MacBook-Air.local>
2024-11-27 22:14:40 +0530

    Added Embedding and Pos Embedding

---
+++ model.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import math 
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model:int, vocab_size:int ):
+        
+        super().__init__()
+        self.d_model = d_model 
+        self.vocab_size = vocab_size 
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model) # multiply sqrt of d_model factor as mentioned in the paper
+
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, d_model:int, seq_len:int, dropout:float) -> None:
+        
+        super().__init__()
+        self.d_model = d_model
+        self.seq.len = seq_len 
+        self.dropout = nn.Dropout(dropout)
+
+        #Create the matrix of (seq_len,d_model)
+        pe = torch.zeros(seq_len,d_model)
+
+        #Create a vector of text length (seq_len, 1)
+        position = torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (- math.log(10000.0) / d_model))
+        
+        #Apply sin to odd and cos to even 
+        pe[:,0::2] = torch.sin(position *  div_term)
+        pe[:,1::2] = torch.cos(position *  div_term)
+
+        #(1, seq_Len, d_model)
+        pe = pe.unsequeeze(0)
+
+        self.register_buffer(pe) 
+
+
+    
+
+    

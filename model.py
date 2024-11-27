487dad2bdcf3a61055055e96564d71b1edaf6aea
Vinoth Loganathan <vinoth@Vinoths-MacBook-Air.local>
2024-11-28 01:11:37 +0530

    Added Attention and Residual Block

--- model.py
+++ model.py
@@ -72,7 +72,7 @@ class LayerNormalization (nn.Module):
 
     def __init__(self, eps:float = 10 ** -6) -> None : 
         
-        super.__init__()
+        super().__init__()
         self.eps = eps 
         self. alpha = nn.Parameter(torch.ones(1)) # Multiplied
         self. bias = nn.Parameter(torch.zeros(1)) # Added 
@@ -88,6 +88,89 @@ class LayerNormalization (nn.Module):
 
         return self.alpha * (x - mean) / (std + self.eps ) + self.bias 
     
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model:int, d_ff:int, dropout:float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model,d_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # W2 and B2
+
+    def forward(self,x):
+        
+        #(Batch = 1, seq_len, d_model) -> (1, seq_len, d_ff) --> (1,seq_len,d_model)
+        return self.linear_2(self.dropout(nn.ReLU(self.linear_1(x))))
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model:int, h:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h 
+        self.dropout = nn.Dropout(dropout)
+
+        assert d_model % h == 0 , "d_model isn't divisble by h"
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) #Wq 
+        self.w_k = nn.Linear(d_model,d_model) #Wk
+        self.w_v = nn.Linear(d_model, d_model) #Wv
+
+        self.w_o = nn.Linear(d_model,d_model) #Wo
+
+    @staticmethod
+    def attention (query, key, value , mask, dropout:nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (Batch, h, seq_len, d_k) -> (Batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+
+        if mask is not None: 
+            attention_scores.masked_fill(mask==0, -1e9)
+        
+        attention_scores = attention_scores.softmax(dim = -1) # (Batch, h, seq_len, seq_len)
+
+        if dropout is not None: 
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value) , attention_scores 
+    
+    def forward( self, q, k, v , mask):
+
+        query = self.w_q(q) #(Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        key = self.w_k(k) #(Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        value = self.w_v(v) #(Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+
+        #(Batch, seq_len, d_model) --> (Batch, seq_len, h, d_k) --transpose--> (Batch,h, seq_len, d_k)
+        #We are trying to split the embedding and learn the attention in the small embedding in parallel and aggregate the learning at the end 
+        query = query.view(query.shape[0],query.shape[1],self.h, self.d_k ).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1],self.h, self.d_k ).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h, self.d_k ).transpose(1,2)
+
+        x, attention_scores = MultiHeadAttentionBlock.attention(query,key,value,mask)
+
+        # (Batch, h, seq_len, d_k ) --> (Batch, seq_len, h, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1,2).contiguous().view(x.shape[0],-1, self.h * self.d_k)
+
+        #(Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+    
+
+class ResidualConnection(nn.module):
+
+    def __init__(self, dropout:float) -> None:
+        super().__init__()
+
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+
+
 
 
 

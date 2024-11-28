fe53eebea7eedcad7b3cb607f186aeba6f7366d3
Vinoth Loganathan <vinoth@Vinoths-MacBook-Air.local>
2024-11-28 11:00:25 +0530

    Added Encoder Block

--- model.py
+++ model.py
@@ -169,6 +169,32 @@ class ResidualConnection(nn.module):
         return x + self.dropout(sublayer(self.norm(x)))
 
 
+class EncoderBlock(nn.module):
+
+    def __init__(self, self_attention_block : MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout:float):
+        super().__init__()
+        self.self_attention_block = self_attention_block 
+        self.feed_forward_block = feed_forward_block 
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range (2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x,src_mask) )
+        x = self.residual_connections[1](x,self.feed_forward_block) 
+
+class Encoder(nn.module):
+
+    def __init__(self , layers:nn.ModuleList):
+        super().__init__()
+        self.layers = layers 
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        
+        return self.norm(x)
+
+
 
 
 
